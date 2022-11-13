2d88cb91674212bf5229dfbd91c69c145e50d684
bumlev <bumwelevy@yahoo.in>
2022-11-13 01:35:47 -0800

    Updates

---
+++ precache-manifest.fa56f8c470ffd0d79c40283eef8c60cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbd968511a50c1d2e0df",
+    "url": "/User-Stories/static/css/main.a70903cb.chunk.css"
+  },
+  {
+    "revision": "dbd968511a50c1d2e0df",
+    "url": "/User-Stories/static/js/main.dbd96851.chunk.js"
+  },
+  {
+    "revision": "bc58c016763d2e895a14",
+    "url": "/User-Stories/static/js/1.bc58c016.chunk.js"
+  },
+  {
+    "revision": "afddcb42be9529219a4f",
+    "url": "/User-Stories/static/js/runtime~main.afddcb42.js"
+  },
+  {
+    "revision": "7e246033cd7e7cda613e731cb4f28f23",
+    "url": "/User-Stories/index.html"
+  }
+];
