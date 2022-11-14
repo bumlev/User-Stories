08f4a3634cf07fd4e88a6d270e3197aa71932ea3
bumlev <bumwelevy@yahoo.in>
2022-11-14 14:41:01 -0800

    commit user stories

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -22,7 +22,7 @@ function Home(){
 
     function goHome(){
         //setIsSearch(false);
-        params= {};
+        params= { };
         console.log('hello');
         navigate(`/home`)
     }
@@ -32,7 +32,7 @@ function Home(){
             <nav>
                 <ul className="list_menu">
                     <li className="Name_project">
-                        <Link  onClick={()=>goHome()}>
+                        <Link to={'/home'}  onClick={()=>goHome()}>
                             <i className="fa fa-github"></i>
                             <span data-testid="git">GitHub API</span>
                         </Link>
