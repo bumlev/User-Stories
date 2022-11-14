f55edb2246fb3ed64ef44d7ca8a86f7c76fdccdd
bumlev <bumwelevy@yahoo.in>
2022-11-14 12:04:08 -0800

    commit user stories

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -21,7 +21,7 @@ function Home(){
     const{ setIsSearch} = useContext(SelectRepositoryContext);
 
     function goHome(){
-        setIsSearch(false);
+        //setIsSearch(false);
         navigate(`/home`);
     }
 
