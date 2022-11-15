f57f12885dbc9aa9777b942066088109983a7774
bumlev <bumwelevy@yahoo.in>
2022-11-14 16:51:05 -0800

    commit user stories

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -25,7 +25,7 @@ function Home(){
     function goHome(){
         //setIsSearch(false);
         console.log('hello');
-        return (<>redirect(`/home`)</>)
+        return (<> <Link to={`/home`}></Link></>)
     }
 
     return (
